bcd49bf77b1cc6c5896665c8cbee549ca6f28c5b
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-08 13:17:51 +0000

    completed program

--- .ccls-cache/@home@runner@Ch6Q1P5/main.c
+++ .ccls-cache/@home@runner@Ch6Q1P5/main.c
@@ -1,6 +1,23 @@
+// Program to determine if a year is a leap year
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  int year, rem4, rem100, rem400;
+
+  printf ("Enter the year to be tested: ");
+  scanf ("%d", &year);
+
+  rem4 = year % 4;
+  rem100 = year % 100;
+  rem400 = year % 400;
+
+  if ( (rem4 == 0 && rem100 != 0) || rem400 == 0 ) {
+    printf ("it is a leap year\n");
+  }
+  else {
+    printf ("Nope, it is not a leap year\n");
+  }
+  
   return 0;
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Programming in C
+## Chapter 6
+### Question 1: Part 5
+
+This problem is from chapter 6 of the textbook, it is to copy and run program 6.5[^1] from the textbook. This program recieves a number from terminal and prints whether it is a leap year.
+
+You may wish to run the program and compare its output.
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 73 - program 6.5
